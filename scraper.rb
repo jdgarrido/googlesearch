c7ba856142855da5497cea083de1d83aadef42e5
José Damián Garrido <damian.garrido.munoz@gmail.com>
2015-01-11 13:29:30 -0300

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -22,3 +22,64 @@
 # on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
+require 'rubygems'
+require 'httparty'
+require 'open-uri'
+require 'nokogiri'
+require 'pp'
+require 'sqlite3'
+require 'htmlentities'
+
+
+words = ['penta','pentagate']
+words_search = words.join("+OR+")
+
+p '+++'
+p 'http://www.google.cl/search?q=allintitle:+'+words_search+'+site:.cl&lr=lang_es&cr=countryCL&biw=1366&bih=613&noj=1&tbs=qdr:d,lr:lang_1es,ctr:countryCL,sbd:1&source=lnt&sa=X&ei=4tKuVML3GouiNsbugLAP&ved=0CBMQpwU'
+p '+++'
+data = Nokogiri::HTML( open('http://www.google.cl/search?q=allintitle:+'+words_search+'+site:.cl&lr=lang_es&cr=countryCL&biw=1366&bih=613&noj=1&tbs=qdr:d,lr:lang_1es,ctr:countryCL,sbd:1&source=lnt&sa=X&ei=4tKuVML3GouiNsbugLAP&ved=0CBMQpwU') )
+data.css('li.g').each do |article|
+	puts '-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.'
+	article.css('h3.r a').each do |link|
+		#pp link['href']
+		puts link.content
+	end
+	article.css('span.st').each do |content|
+		puts '	' + content.content
+	end
+end
+
+db = SQLite3::Database.new "data.sqlite"
+db.execute <<-SQL
+
+	DROP TABLE IF EXISTS data_searches;
+SQL
+
+db.execute <<-SQL
+
+	CREATE TABLE data_searches (
+		idx INTEGER PRIMARY KEY AUTOINCREMENT,
+		url VARCHAR(512),
+		title VARCHAR(256),
+		content VARCHAR(256)
+	);
+
+SQL
+
+coder = HTMLEntities.new
+
+data.css('li.g').each do |article|
+	article.css('h3.r a').each do |link|
+		@data_link = link.content['href']
+		@data_title_link = link.content
+	end
+	article.css('span.st').each do |content|
+		@data_content = content.content.to_s
+	end
+
+	db.execute( 'INSERT INTO data_searches (url, title, content) VALUES (?,?,?)', @data_link, @data_title_link, @data_content )
+end
+
+db.execute( "select * from data_searches" ) do |row|
+  p row
+end
